fb7c2c20e791ac22a758cbe9cb1f6c63c8d072c6
Rasuli9687 <fraidoonrasuli01@gmail.com>
2022-12-21 18:54:20 -0500

    I have succesfully add the code

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main {
         Fxron.size="4382742km";
         Fxron.temp="-23478274372k";
 
+        Planet1 Stuart=new Planet1();
+        Stuart.color="white";
+        Stuart.size=232323;
+        Stuart.rings="5";
+        Stuart.temperature="-3434343434";
 
 
     }
